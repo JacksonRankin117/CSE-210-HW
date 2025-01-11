fbf84d70dc8ecdb8a6c64e7c8fe6827bfb3f1297
JacksonRankin117 <ran23008@byui.edu>
2025-01-11 15:24:54 -0700

    Completed the prep activity

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,30 @@
+/*
+Jackson Rankin
+CSE 210
+Brother Gibbons
+11th January, 2025
+
+This program asks the user for their first name and surname, then displays the
+output text to the terminal
+
+*/
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    {   
+        // Asks the user for his or her first name
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+
+        // Asks the user for his or her surname
+        Console.WriteLine("What is your surname?");
+        string surname = Console.ReadLine();
+
+        // Outputs display text to the terminal, i.e:
+        // Your name is Bond, James Bond
+        Console.WriteLine($"Your name is {surname}, {firstName} {surname}.");
     }
 }
