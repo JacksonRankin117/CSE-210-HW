af3b580526125ce8e22a3357a494139a73b5f17c
JacksonRankin117 <ran23008@byui.edu>
2025-02-07 18:42:31 -0700

    Just messing around so far. Started on fraction

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Scripture
+{
+    List<string> WordList = new List<string>();
+    List<string> HiddenWordList = new List<string>();
+
+    public void HiddenRandomWord() 
+    {
+
+    }
+
+    public void Display() 
+    {
+
+    }
+    public void AllWordsHidden(bool ) {
+
+    }
+}
